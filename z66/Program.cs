1b5679e38ff3ecea943b652836e14a581a13973e
Arteeex <arteeex187@gmail.com>
2023-02-20 11:08:55 +0700

    z66

---
+++ z66/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int m = InputInt("Введите M: ");
+int n = InputInt("Введите N: ");
+Console.WriteLine($"Сумма элементов от {m} до {n} = {NaturalSum(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int NaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + NaturalSum(m, n - 1);
+}
